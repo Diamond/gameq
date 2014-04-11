e7af67535997b88c2f5c87cae8f7fa05f9fe7bed
Brandon Richey <brichey@teladoc.com>
2014-04-11 12:02:32 -0400

    Updates and junk

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "When creating a new user account" do
+    context "With valid data" do
+      subject { FactoryGirl.build(:user) }
+      it { should be_valid }
+    end
+
+    context "With invalid data" do
+      it "is invalid without an email"
+      it "is invalid without a password"
+      it "is invalid if the password and password confirmation do not match"
+      it "is invalid if the email is already registered"
+    end
+  end
 end
