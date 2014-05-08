9e6b6e21845c4af55db939e9d2e0f3146d0ce99b
Brandon Richey <brichey@teladoc.com>
2014-05-07 22:07:19 -0400

    Adding sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,12 @@
 class SessionsController < ApplicationController
   def create
+    @user = login params[:email], params[:password]
+    session[:user_id] = @user.present? ? @user.id : nil
+    head (@user.present? ? :ok : :unprocessable_entity)
   end
 
   def destroy
+    session[:user_id] = nil
+    head :ok
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,9 +3,16 @@ require 'spec_helper'
 describe SessionsController do
 
   describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
+    it "returns http success with valid credentials" do
+      @user = FactoryGirl.create(:user, email: 'test@test.com', password: 'test1234')
+      get 'create', email: 'test@test.com', password: 'test1234'
       response.should be_success
+      @user.destroy
+    end
+
+    it "returns unprocessable entity with invalid credentials" do
+      get 'create'
+      response.should_not be_success
     end
   end
 
@@ -15,5 +22,4 @@ describe SessionsController do
       response.should be_success
     end
   end
-
 end
