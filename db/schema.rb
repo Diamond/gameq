efc333fb6e1f6fcabb498ad69810d71e1408b86b
Brandon Richey <brichey@teladoc.com>
2014-05-12 16:11:41 -0400

    Adding console db structure

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140510040732) do
+ActiveRecord::Schema.define(version: 20140512201118) do
+
+  create_table "consoles", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "games", force: true do |t|
     t.string   "name"
